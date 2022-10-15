55797d1f997bc8266f0da3adc18032272f113032
dbdpfls <year9882@gmail.com>
2022-10-15 15:41:28 +0900

    * 2022.10.15 - 니모닉 코드 생성 - 지갑 주소 생성 (진행중)

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,14 @@
 export default {
-    async setA({ commit }, payload) {
-        commit('SET_A', payload);
+    async SET_Mnemonic({ commit }, payload) {
+        commit('SET_Mnemonic', payload);
+    },
+    async SET_PWD({ commit }, payload) {
+        commit('SET_PWD', payload);
+    },
+    async CHK_Mnemonic({ commit }, payload) {
+        commit('CHK_Mnemonic', payload);
+    },
+    async SET_WALLET({ commit }, payload) {
+        commit('SET_WALLET', payload);
     }
 }
