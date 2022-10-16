aaddfcc93add8b32b72cf2c4dfd2f1f93561d0aa
dbdpfls <year9882@gmail.com>
2022-10-16 21:35:47 +0900

    BTA1stProject-08-6 03 로그인

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -5,8 +5,11 @@ export default {
     SET_PWD(state, val){
         state.pwd = val;
     },
+    CHK_PWD(state, val){
+        state.loginPwd = val;
+    },
     CHK_Mnemonic(state, val){
-        state.pwd = val;
+        state.mnemonicChk = val;
     },
     SET_WALLET(state, val){
         state.wallet = val;
--- src/views/CreateAccountView.vue
+++ src/views/CreateAccountView.vue
@@ -4,6 +4,8 @@
 
     <span>공개키 : {{ publicKey }}</span><br><br>
     <span>지갑 주소 : {{ address }}</span>
+
+    <button class="btn-hover color-5"><router-link to="/login" v-bind:style="{color: 'white'}">로그인</router-link></button>
   </div>
 </template>
 
